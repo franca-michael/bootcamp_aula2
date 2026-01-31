cec6b131ea7d8e02005cddef10e0f70c483ec359
Mike-lnx <michael.mjpr@gmail.com>
2026-01-31 15:38:56 -0300

    Exercícios 1 ao 5 resolvidos

--- exercicios.py
+++ exercicios.py
@@ -10,7 +10,16 @@ print(resultado)
 
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero = int(input("Inserir um numero inteiro: "))
+resto = numero % 5
+print(resto)
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir outro numero inteiro: "))
+resultado = numero_01 * numero_02
+print(resultado)
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 numero_01 = int(input("Inserir um numero inteiro: "))
@@ -19,6 +28,9 @@ resultado = numero_01 // numero_02
 print(resultado)
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero = int(input("Inserir um numero inteiro: "))
+quadrado = numero ** 2
+print(quadrado)
 
 # #### Números de Ponto Flutuante (`float`)
 
